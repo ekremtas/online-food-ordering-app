6660b125dc2fb6f4f7e763a58b60a9f221d8c1e7
ekremtas <ekremtas589@gmail.com>
2019-07-11 14:19:39 +0300

    second commit

--- Yemekkutusu/uye_ol.aspx.cs
+++ Yemekkutusu/uye_ol.aspx.cs
@@ -26,7 +26,18 @@ namespace Yemekkutusu
         {
             SqlCommand cmd = sqlCon.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "insert into uye (uye_email,uye_ad,uye_soyad,uye_telefon,uye_dt,uye_parola,uye_adres) values('" + email_TextBox.Text + "','" + ad_TextBox.Text  +"','"+ Soyad_TextBox.Text + "','" + telefon_TextBox.Text + "','"+ txtDate.Text +"','" + parola_TextBox.Text + "','" + adres_TextBox.Text +"')";
+            cmd.CommandText = "insert into adres (adres_il,adres_ilçe,adres_semt) values('" + email_TextBox.Text + "','" + ad_TextBox.Text + "','" + Soyad_TextBox.Text + "')";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "select adres_id from adres where adres_il='" + email_TextBox.Text + "' and adres_ilçe='" + ad_TextBox.Text + "' and adres_semt='" + Soyad_TextBox.Text + "'";
+            cmd.Connection = sqlCon;
+            SqlDataReader read = cmd.ExecuteReader();
+            String adres__id= string.Empty;
+            if (read.Read() == true)
+                {
+                    adres__id = read["adres_id"].ToString();
+                }
+            read.Close();
+            cmd.CommandText = "insert into uye (uye_email,uye_ad,uye_soyad,uye_telefon,uye_dt,uye_parola,uye_adres,adres_id) values('" + email_TextBox.Text + "','" + ad_TextBox.Text  +"','"+ Soyad_TextBox.Text + "','" + telefon_TextBox.Text + "','"+ Calendar1.ToString() +"','" + parola_TextBox.Text + "','" + adres_TextBox.Text + "'," + adres__id + ")";
             cmd.ExecuteNonQuery();
 
 
